09f002afcc1f07270087115a788f28f3c92b4513
marcus-wrrn <marcuswrrn@gmail.com>
2022-02-20 16:13:46 -0500

    Started overhauling the Controller Structure

--- Game1.cs
+++ Game1.cs
@@ -7,24 +7,21 @@ using MenuSystem;
 namespace MapBuilder
 {
     public class Game1 : Game
-    {
-        private Texture2D ball;
-        
+    {   
         private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-
-        private Background background;
-        private TilePickerMenu menu;
-        private Drawing.Brush brush;
+        public SpriteBatch _spriteBatch;
+        public Background background;
+        public TilePickerMenu TileMenu;
+        public Drawing.Brush brush;
+        private Controller.GameContainer gameContainer;
         private string fileName = "test";
-
         private Controller.GameControl controller;
-
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             _graphics.PreferredBackBufferWidth = 3840;
             _graphics.PreferredBackBufferHeight = 2160;
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -33,68 +30,43 @@ namespace MapBuilder
         { 
             base.Initialize();
             controller = new Controller.GameControl();
+            Texture2D[] test = new Texture2D[8];
+            for(int i = 0; i < test.Length; i++)
+                test[i] = Content.Load<Texture2D>("tile2Test");
+            test[0] = Content.Load<Texture2D>("tile");
+            Texture2D menuTexture = Content.Load<Texture2D>("Menu");
+            Background background = new Background(fileName, this);
+            // background = new Background(currentTile, (int)(_graphics.PreferredBackBufferHeight / currentTile.Height), (int)(_graphics.PreferredBackBufferWidth / currentTile.Width));
+            MenuSystem.TilePickerMenu menu = new TilePickerMenu(test, 0, new Vector2(_graphics.PreferredBackBufferWidth - menuTexture.Width, 0), menuTexture);
+            Drawing.Brush brush = new Drawing.Brush(Content.Load<Texture2D>("tile"), Content.Load<Texture2D>("tile2Test"));
+            gameContainer = new Controller.GameContainer(menu, background, brush);
         }
 
         protected override void LoadContent()
         {
-            Texture2D[] test = new Texture2D[8];
-            for(int i = 0; i < test.Length; i++) {
-                test[i] = Content.Load<Texture2D>("tile2Test");
-            }
-            test[0] = Content.Load<Texture2D>("tile");
-
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            ball = Content.Load<Texture2D>("Ball");
-            Texture2D menuTexture = Content.Load<Texture2D>("Menu");
-            background = new Background(fileName, this);
-            // background = new Background(currentTile, (int)(_graphics.PreferredBackBufferHeight / currentTile.Height), (int)(_graphics.PreferredBackBufferWidth / currentTile.Width));
-            menu = new TilePickerMenu(test, 0, new Vector2(_graphics.PreferredBackBufferWidth - menuTexture.Width, 0), menuTexture);
-            brush = new Drawing.Brush(Content.Load<Texture2D>("tile"), Content.Load<Texture2D>("tile2Test"));
+            
         }
 
         protected override void Update(GameTime gameTime)
         {
+            // Update Containers
+            // Update Controllers
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-
-            var kstate = Keyboard.GetState();
-
             // Command Logic                
-            controller.MenuEffects(this, menu, gameTime);
+            controller.CommandInputs(this, gameTime);
             // Mouse Logic
             // Finds the position of the mouse
             Vector2 mouse = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
-            Vector2 mouseStart = new Vector2(0,0);
             if (mouse.X <= _graphics.PreferredBackBufferWidth && mouse.Y <= _graphics.PreferredBackBufferHeight)
-                brush.UpdateBrush(background, mouse);
-            // If left mouse button pressed
-            if(Mouse.GetState().LeftButton == ButtonState.Pressed) {
-                // Check to see if it clicked on a menu icon
-                Texture2D tempTile = menu.GetTileTexture(mouse);
-                if(tempTile != null)
-                    brush.PrimaryTexture.Texture = tempTile;
-                else if(menu.IfMenuClicked(mouse)) {
-                    if(!menu.MenuClicked) {
-                        System.Console.WriteLine("WTF");
-                        menu.MenuClicked = true;
-                        mouseStart.X = mouse.X;
-                        mouseStart.Y = mouse.Y;
-                    }
-                    else  {
-                        System.Console.WriteLine("It got here!");
-                        //menu.DragAndDrop(mouse, mouseStart, gameTime);
-                    }
-                }    
-                // If the menu icon wasn't clicked update the tile below it
-                else {
-                    background.UpdateTile(brush.PrimaryTexture.Texture, mouse);
-                    menu.MenuClicked = false;
-                }
-            }
+                gameContainer.Brush.UpdateBrush(gameContainer.Map, mouse);
+            
+            var inputContainer = new Controller.InputContainer(Mouse.GetState(), Keyboard.GetState(), gameTime);
+            controller.Update(this, gameTime, gameContainer, inputContainer);
             // If e was pressed export the map to a binary file
             if(Keyboard.GetState().IsKeyDown(Keys.E))
-                background.ExportToBinary(fileName);
+                gameContainer.Map.ExportToBinary(fileName);
 
             base.Update(gameTime);
         }
@@ -103,9 +75,11 @@ namespace MapBuilder
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
-            background.Draw(_spriteBatch);
-            brush.DrawBrush(_spriteBatch);
-            menu.Draw(_spriteBatch);
+
+            gameContainer.Map.Draw(_spriteBatch);
+            gameContainer.Brush.DrawBrush(_spriteBatch);
+            gameContainer.TileMenu.Draw(_spriteBatch);
+
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
